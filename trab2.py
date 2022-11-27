5e0d235e6d60db97384ab2bba94be69680a2b014
lucas-mior <lucas.mior@tutamail.com>
2022-11-27 20:14:30 -0300

    L_b1 -> L1

--- trab2.py
+++ trab2.py
@@ -9,22 +9,22 @@ print("\n## Questão 1: Sorteio com 20 trios de demanda e afluência ##")
 media = 25  # mu, demanda
 desvio_padrao = 2  # sigma, variação na demanda
 
-# L_b1 = [0] * 20
-L_b1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+# L1 = [0] * 20
+L1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-print(f"Demanda barra 1 L_b1: {L_b1}")
+print(f"Demanda barra 1 L1: {L1}")
 
-# L_b2 = list(np.random.normal(media, desvio_padrao, 20))
-# L_b2 = [round(item) for item in L_b2]
-L_b2 = [24, 24, 25, 22, 22, 25, 24, 29, 25, 25,
+# L2 = list(np.random.normal(media, desvio_padrao, 20))
+# L2 = [round(item) for item in L2]
+L2 = [24, 24, 25, 22, 22, 25, 24, 29, 25, 25,
         26, 25, 26, 30, 23, 24, 25, 25, 27, 25]
-print(f"Demanda barra 2 L_b2: {L_b2}")
+print(f"Demanda barra 2 L2: {L2}")
 
-# L_b3 = list(np.random.normal(media, desvio_padrao, 20))
-# L_b3 = [round(item) for item in L_b3]
-L_b3 = [28, 27, 23, 25, 25, 27, 23, 27, 24, 28,
+# L3 = list(np.random.normal(media, desvio_padrao, 20))
+# L3 = [round(item) for item in L3]
+L3 = [28, 27, 23, 25, 25, 27, 23, 27, 24, 28,
         22, 25, 22, 24, 24, 24, 27, 21, 26, 26]
-print(f"Demanda barra 3 L_b3: {L_b3}")
+print(f"Demanda barra 3 L3: {L3}")
 
 # Y = list(np.random.uniform(0, 100, 20))
 # Y = [round(item) for item in Y]
@@ -36,7 +36,7 @@ print(f"Afluência Y: {Y}")
 print("\n## Questão 2: Para cada trio sorteado, obter o despacho ótimo")
 
 
-def roda_caso(L_b1, L_b2, L_b3, Y):
+def roda_caso(L1, L2, L3, Y):
     m = Model('Custo')
     inf = GRB.INFINITY
     gt1 = m.addVar(lb=0,   ub=30,     name='gt1')
@@ -55,9 +55,9 @@ def roda_caso(L_b1, L_b2, L_b3, Y):
     m.addConstr(y == Y, 'afluência')
     m.addConstr(v + q + s == y, 'balanço de volume')
     m.addConstr(gh == 0.9*q + 0.1*v, 'balanço de volume')
-    m.addConstr(gh - f12 - f13 == L_b1, 'barra 1')
-    m.addConstr(gt1 + gt2 + f12 + f32 == L_b2, 'barra 2')
-    m.addConstr(gt3 + f13 - f32 == L_b3, 'barra 3')
+    m.addConstr(gh - f12 - f13 == L1, 'barra 1')
+    m.addConstr(gt1 + gt2 + f12 + f32 == L2, 'barra 2')
+    m.addConstr(gt3 + f13 - f32 == L3, 'barra 3')
     m.addConstr(alfa + 6*v >= 287.5, 'corte 1')
     m.addConstr(alfa + 4*v >= 237.5, 'corte 2')
     m.addConstr(alfa + 1.5*v >= 112.5, 'corte 3')
@@ -95,13 +95,13 @@ def CMO(custo_base, D1, D2, D3, Y):
 print("\nDespacho ótimo: ")
 opt = []
 for trio in range(20):
-    r = roda_caso(L_b1[trio], L_b2[trio], L_b3[trio], Y[trio])
+    r = roda_caso(L1[trio], L2[trio], L3[trio], Y[trio])
     print(f"Período {trio}:")
     pp.pprint(r)
     opt.append(r)
 
 for trio in range(20):
-    linha = CMO(opt[trio]['custo'], L_b1[trio], L_b2[trio], L_b3[trio], Y[trio])
+    linha = CMO(opt[trio]['custo'], L1[trio], L2[trio], L3[trio], Y[trio])
     opt[trio]['cmo1'] = linha[0]
     opt[trio]['cmo2'] = linha[1]
     opt[trio]['cmo3'] = linha[2]
@@ -114,27 +114,27 @@ for trio in range(20):
     b3 = opt[trio]['cmo3'] * (opt[trio]['gt1'] + opt[trio]['gt2'])
     opt[trio]['custo_receita'] = round(b1 + b2 + b3, 2)
     CMO = max(opt[trio]['cmo1'], opt[trio]['cmo2'], opt[trio]['cmo3'])
-    L = L_b1[trio] + L_b2[trio] + L_b3[trio]
+    L = L1[trio] + L2[trio] + L3[trio]
     opt[trio]['custo_mercado'] = round(CMO*L, 2)
     opt[trio]['EM'] = opt[trio]['custo_mercado'] - opt[trio]['custo']
     opt[trio]['EMT'] = opt[trio]['custo_mercado'] - opt[trio]['custo_receita']
 
 df = pd.DataFrame.from_records(opt)
 print(df)
-df['L_b1'] = L_b1
-df['L_b2'] = L_b2
-df['L_b3'] = L_b3
+df['L1'] = L1
+df['L2'] = L2
+df['L3'] = L3
 
 # pp.pprint(opt)
 
 print("\n## Questão 4: ##")
 # sem contratos
-contr = pd.DataFrame(columns=['gt1', 'gt2', 'gt3', 'gh', 'L_b2', 'L_b3'])
+contr = pd.DataFrame(columns=['gt1', 'gt2', 'gt3', 'gh', 'L2', 'L3'])
 zeros = [0, 0, 0, 0, 0, 0]
 contr.loc[0] = zeros
 
 # com contratos
-contr2 = pd.DataFrame(columns=['gt1', 'gt2', 'gt3', 'gh', 'L_b2', 'L_b3'])
+contr2 = pd.DataFrame(columns=['gt1', 'gt2', 'gt3', 'gh', 'L2', 'L3'])
 contr2.loc[0] = [15, 0, 0, 10, 0, 0]
 
 
@@ -146,8 +146,8 @@ def contabilizacao(opt, contrato):
     df_mcp['MCP_gt3'] = (opt.gt3 - contrato.gt3.loc[0]) * opt.cmo3.values
     df_mcp['MCP_gh'] = (opt.q - contrato.gt1.loc[0]) * opt.cmo1.values
     # consumidores
-    df_mcp['MCP_B2'] = (opt.L_b2 - contrato.L_b2.loc[0]) * opt.cmo2.values
-    df_mcp['MCP_B3'] = (opt.L_b3 - contrato.L_b3.loc[0]) * opt.cmo3.values
+    df_mcp['MCP_B2'] = (opt.L2 - contrato.L2.loc[0]) * opt.cmo2.values
+    df_mcp['MCP_B3'] = (opt.L3 - contrato.L3.loc[0]) * opt.cmo3.values
     a = df_mcp[['MCP_gt1', 'MCP_gt2', 'MCP_gt3', 'MCP_gh']].sum(axis=1)
     b = -df_mcp[['MCP_B2', 'MCP_B3']].sum(axis=1).values
     df_mcp['EMT'] = a + b
